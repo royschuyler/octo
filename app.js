73a30a7acd2e847b038b1e27476da570e3c6c193
royschuyler <royschuyler@yahoo.com>
2015-08-19 12:29:58 -0500

    posting and recieving from mongo

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var app = express();
+var mongo = require('mongodb').MongoClient;
+var bodyParser = require('body-parser')
+
+app.use(bodyParser());
+
+
+
+
+
+mongo.connect('mongodb://localhost:27017/meow', function(err, db) {
+  global.db = db;
+});
+
+
+
+//-----------------------------------
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/home', function (req, res) {
+  res.render('./home.ejs', {name: "Roy Schuyler"});
+});
+
+//----------------------------------------------------
+app.get('/display', function (req, res) {
+  var collection = global.db.collection("meow")
+  collection.find().toArray(function(err, meows) {
+    newMeows = meows.map(function(meow) {
+      return {
+        _id: meow._id,
+        name: meow.name,
+      };
+    });
+    res.render('display.ejs', {meows: newMeows});
+    console.log(newMeows)
+  });
+});
+
+//----------------------------------------------------
+app.get('/post', function (req, res) {
+  res.render('post.ejs');
+});
+
+app.post('/post', function(req, res) {
+  var collection = global.db.collection('meow')
+  collection.save(req.body)
+
+  res.redirect('/display')
+  });
+
+
+
+//------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
